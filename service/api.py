64668e217c21af1229db68c084d3523f709787be
T <taylor@localcoinswap.com>
2020-06-17 17:52:48 +1000

    Start apis

--- service/api.py
+++ service/api.py
@@ -14,7 +14,7 @@ GETBALANCE = [{"name": "address", "type": str, "required": True}]
 
 GETNONCE = [{"name": "address", "type": str, "required": True}]
 
-GETEVENTS = [{"name": "block_hash", "type": str, "required": True}]
+# GETEVENTS = [{"name": "block_hash", "type": str, "required": True}]
 
 GETEXTRINSICHASH = [{"name": "final_transaction", "type": str, "required": True}]
 
@@ -129,6 +129,170 @@ class GetBalance(Resource):
         return kusama.get_balance(address)
 
 
+class GetNonce(Resource):
+    """
+    Returns the free balance associated with provided address
+    """
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        for parameter in GETNONCE:
+            self.reqparse.add_argument(
+                parameter["name"],
+                type=parameter["type"],
+                required=parameter["required"],
+                action="store",
+            )
+        super(GetNonce, self).__init__()
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        address = args["address"]
+
+        return kusama.get_nonce(address)
+
+
+class GetExtrinsicHash(Resource):
+    """
+    Returns the extrinsic hash from provided final transaction
+    """
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        for parameter in GETEXTRINSICHASH:
+            self.reqparse.add_argument(
+                parameter["name"],
+                type=parameter["type"],
+                required=parameter["required"],
+                action="store",
+            )
+        super(GetExtrinsicHash, self).__init__()
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        final_transaction = args["final_transaction"]
+
+        return kusama.get_extrinsic_hash(final_transaction)
+
+
+class GetExtrinsicTimepoint(Resource):
+    """
+    Returns the extrinsic timepoint from provided final transaction
+    """
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        for parameter in GETEXTRINSICTIMEPOINT:
+            self.reqparse.add_argument(
+                parameter["name"],
+                type=parameter["type"],
+                required=parameter["required"],
+                action="store",
+            )
+        super(GetExtrinsicTimepoint, self).__init__()
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        final_transaction = args["final_transaction"]
+        node_response = args["node_response"]
+        return kusama.get_extrinsic_timepoint(node_response, final_transaction)
+
+
+class GetExtrinsicEvents(Resource):
+    """
+    Returns the extrinsic timepoint from provided final transaction
+    """
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        for parameter in GETEXTRINSICEVENTS:
+            self.reqparse.add_argument(
+                parameter["name"],
+                type=parameter["type"],
+                required=parameter["required"],
+                action="store",
+            )
+        super(GetExtrinsicEvents, self).__init__()
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        block_hash = args["block_hash"]
+        extrinsinc_index = args["extrinsinc_index"]
+        return kusama.get_extrinsic_events(block_hash, extrinsinc_index)
+
+
+class GetEscrowAddress(Resource):
+    """
+    Returns the extrinsic timepoint from provided final transaction
+    """
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        for parameter in GETESCROWADDRESS:
+            self.reqparse.add_argument(
+                parameter["name"],
+                type=parameter["type"],
+                required=parameter["required"],
+                action="store",
+            )
+        super(GetEscrowAddress, self).__init__()
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        buyer_address = args["buyer_address"]
+        seller_address = args["seller_address"]
+
+        return kusama.get_escrow_address(buyer_address, seller_address)
+
+
+class GetBlockHash(Resource):
+    """
+    Returns the extrinsic timepoint from provided final transaction
+    """
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        for parameter in GETBLOCKHASH:
+            self.reqparse.add_argument(
+                parameter["name"],
+                type=parameter["type"],
+                required=parameter["required"],
+                action="store",
+            )
+        super(GetBlockHash, self).__init__()
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        node_response = args["node_response"]
+
+        return kusama.get_block_hash(node_response)
+
+
+class TransferPayload(Resource):
+    """
+    Returns the extrinsic timepoint from provided final transaction
+    """
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        for parameter in TRANSFERPAYLOAD:
+            self.reqparse.add_argument(
+                parameter["name"],
+                type=parameter["type"],
+                required=parameter["required"],
+                action="store",
+            )
+        super(TransferPayload, self).__init__()
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        from_address = args["from_address"]
+        to_address = args["to_address"]
+        value = args["value"]
+
+        return kusama.transfer_payload(from_address, to_address, value)
+
+
 class HeartBeat(Resource):
     """
     Are we alive?
@@ -140,3 +304,11 @@ class HeartBeat(Resource):
 
 def get_resources(api):
     api.add_resource(HeartBeat, "/HeartBeat")
+    api.add_resource(GetBalance, "/GetBalance")
+    api.add_resource(GetNonce, "/GetNonce")
+    api.add_resource(GetExtrinsicHash, "/GetExtrinsicHash")
+    api.add_resource(GetExtrinsicTimepoint, "/GetExtrinsicTimepoint")
+    api.add_resource(GetExtrinsicEvents, "/GetExtrinsicEvents")
+    api.add_resource(GetEscrowAddress, "/GetEscrowAddress")
+    api.add_resource(GetBlockHash, "/GetBlockHash")
+    api.add_resource(TransferPayload, "/TransferPayload")
--- service/middleware.py
+++ service/middleware.py
@@ -5,5 +5,5 @@ from .settings import NODE_PROVIDER
 
 kusama = Kusama(arbitrator_key=ARBITRATOR_KEY, node_url=NODE_PROVIDER)
 
-assert kusama.connect()
-assert kusama.runtime_info()
+kusama.connect()
+kusama.runtime_info()
