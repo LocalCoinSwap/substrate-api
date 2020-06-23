1329beacd80a8eb556cc62000b58dc148fe72a8e
Ravi Ojha <ivarojha@gmail.com>
2020-06-23 15:50:41 +0530

    All the config files used so far for correct deployment
     - files under kubernetes/ were used for setting up
       cluster, iam role and policy, ingress deployment,
       and service deployment
     - moved some docker files outside because there were
       issues with them lying inside, I think this can be
       fixed later once we get everything working

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from flask_script import Server
 from service.api import get_resources
 from service.application import create_application
 from service.settings import DEBUG_STATUS
-from service.settings import PORTS
-from service.settings import SERVICE_NAME
+from service.settings import HOST
+from service.settings import PORT
 
 app = create_application()
 manager = Manager(app)
@@ -17,9 +17,7 @@ if __name__ == "__main__":
     manager.add_command(
         "runserver",
         Server(
-            use_debugger=DEBUG_STATUS,
-            use_reloader=DEBUG_STATUS,
-            port=PORTS[SERVICE_NAME],
+            host=HOST, port=PORT, use_debugger=DEBUG_STATUS, use_reloader=DEBUG_STATUS,
         ),
     )
     manager.run()
