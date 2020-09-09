be3b43bfd101b3d52ab5e81e321ddf7417cd7554
Ravi Ojha <ivarojha@gmail.com>
2020-09-09 16:31:08 +0530

    Remove print statement

--- service/middleware.py
+++ service/middleware.py
@@ -3,7 +3,6 @@ from ksmutils import Kusama
 from .settings import ARBITRATOR_KEY
 from .settings import NODE_PROVIDER
 
-print("-" * 90)
 print(NODE_PROVIDER)
 kusama = Kusama(arbitrator_key=ARBITRATOR_KEY, node_url=NODE_PROVIDER)
 kusama.connect()
