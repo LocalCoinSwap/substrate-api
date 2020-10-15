0a7efb6d8331625f21ab22ba92ca168090e36520
codeforcer <nathancworsley@gmail.com>
2020-10-15 19:46:42 +0100

    Revert "Update prod to public node"
    This reverts commit 010e418c8355b66891928ebf1d17aa90c08568ee.

--- service/config/production.py
+++ service/config/production.py
@@ -23,4 +23,4 @@ INTERNAL_ONLY = False
 PORT = 443
 HOST = "0.0.0.0"
 ARBITRATOR_KEY = params["ARBITRATOR_HEX_SEED"]
-NODE_PROVIDER = "wss://kusama-rpc.polkadot.io/"
+NODE_PROVIDER = "ws://ksmnode.lcs.internal/"
