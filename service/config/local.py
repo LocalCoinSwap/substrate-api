5a0fd93de14cfe671e04723cc665ee059eae7119
codeforcer <nathancworsley@gmail.com>
2020-11-14 12:08:53 +0000

    Remove .env from dockerignore so it goes in local. Fix docker issues with loadenv Python package. Sacffold in the Polkadot setup. Add Rust to local docker build so it works

--- service/config/local.py
+++ service/config/local.py
@@ -1,11 +1,8 @@
 import os
-from pathlib import Path
 
 from dotenv import load_dotenv
 
-env_path = Path(".") / ".env"
-load_dotenv(dotenv_path=env_path)
-
+load_dotenv()
 UNIFIED_LOGGING = False
 DEBUG_STATUS = True
 LOGSTASH = {"host": "localhost", "port": 9601, "name": "kusama_api_logger"}
--- service/middleware.py
+++ service/middleware.py
@@ -1,17 +1,18 @@
 from substrateutils import Kusama
-from substrateutils import Polkadot
 
 from .settings import ARBITRATOR_KEY
 from .settings import KUSAMA_NODE_PROVIDER
 from .settings import POLKADOT_NODE_PROVIDER
+
+# from substrateutils import Polkadot
 
 print("Kusama provider", KUSAMA_NODE_PROVIDER)
 print("Polkadot provider", POLKADOT_NODE_PROVIDER)
 
 kusama = Kusama(arbitrator_key=ARBITRATOR_KEY, node_url=KUSAMA_NODE_PROVIDER)
 kusama.connect()
-kusama.runtime_info()
+print("Kusama runtime", kusama.runtime_info()["specVersion"])
 
-polkadot = Polkadot(arbitrator_key=ARBITRATOR_KEY, node_url=POLKADOT_NODE_PROVIDER)
-polkadot.connect()
-polkadot.runtime_info()
+# polkadot = Polkadot(arbitrator_key=ARBITRATOR_KEY, node_url=POLKADOT_NODE_PROVIDER)
+# polkadot.connect()
+# print("Polkadot runtime", polkadot.runtime_info()['specVersion'])
