6e95016a96fa98303a0e63fb09086bbb92819c49
codeforcer <nathancworsley@gmail.com>
2020-11-14 12:24:16 +0000

    bump versioning

--- service/middleware.py
+++ service/middleware.py
@@ -1,10 +1,9 @@
 from substrateutils import Kusama
+from substrateutils import Polkadot
 
 from .settings import ARBITRATOR_KEY
 from .settings import KUSAMA_NODE_PROVIDER
 from .settings import POLKADOT_NODE_PROVIDER
-
-# from substrateutils import Polkadot
 
 print("Kusama provider", KUSAMA_NODE_PROVIDER)
 print("Polkadot provider", POLKADOT_NODE_PROVIDER)
@@ -13,6 +12,6 @@ kusama = Kusama(arbitrator_key=ARBITRATOR_KEY, node_url=KUSAMA_NODE_PROVIDER)
 kusama.connect()
 print("Kusama runtime", kusama.runtime_info()["specVersion"])
 
-# polkadot = Polkadot(arbitrator_key=ARBITRATOR_KEY, node_url=POLKADOT_NODE_PROVIDER)
-# polkadot.connect()
-# print("Polkadot runtime", polkadot.runtime_info()['specVersion'])
+polkadot = Polkadot(arbitrator_key=ARBITRATOR_KEY, node_url=POLKADOT_NODE_PROVIDER)
+polkadot.connect()
+print("Polkadot runtime", polkadot.runtime_info()["specVersion"])
